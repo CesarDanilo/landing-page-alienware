dd47d5084e7a8362ce44062338df46f2217ac437
Cesar Danilo <billypalacios616@gmail.com>
2023-05-26 12:51:13 -0300

    adicionando funcionalidades de reponsividade e funções em js

--- landing page alienware/src/html/app.js
+++ landing page alienware/src/html/app.js
@@ -1,7 +1,6 @@
 const btn = document.querySelector(".btn-enviar"); // BOTÃO ENVIAR
 const email = document.querySelector("#email"); // VARIAVEL E-MAIL
 const nome = document.querySelector("#nome"); // VARIAVEL NOME
-
 const btnComprar = document.querySelector(".btn-comprar"); // BOTÃO DE COMPRAR
 
 // VALIDAÇÃO NO CAMPO ENTRAR EM CONTATO 
@@ -12,10 +11,10 @@ function ClickEnvent(email, nome) {
             email.value = '';
             nome.value = '';
         } else {
-            swal("Campo 'Nome' esta vazio !");
+            swal("Campo Nome esta vazio !");
         }
     } else {
-        swal("Campo 'E-mail' esta vazio !");
+        swal("Campo E-mail esta vazio !");
     }
 };
 
@@ -23,3 +22,11 @@ btn.addEventListener('click', function () {
     ClickEnvent(email, nome);
 })
 
+
+function ClickComprar() {
+    swal("FUNÇÃO AINDA NÃO CRIADA")
+}
+
+btnComprar.addEventListener('click', function () {
+    ClickComprar();
+});
