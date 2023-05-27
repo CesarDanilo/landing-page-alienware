89fc86f691e764a8e7556e9b0a4fc4a0bd23b097
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2023-05-27 09:32:38 -0400

    Add files via upload

---
+++ landing page alienware/app.js
@@ -0,0 +1,32 @@
+const btn = document.querySelector(".btn-enviar"); // BOTÃO ENVIAR
+const email = document.querySelector("#email"); // VARIAVEL E-MAIL
+const nome = document.querySelector("#nome"); // VARIAVEL NOME
+const btnComprar = document.querySelector(".btn-comprar"); // BOTÃO DE COMPRAR
+
+// VALIDAÇÃO NO CAMPO ENTRAR EM CONTATO 
+function ClickEnvent(email, nome) {
+    if (email.value) {
+        if (nome.value) {
+            swal("Mensagem Enviada com súcesso!");
+            email.value = '';
+            nome.value = '';
+        } else {
+            swal("Campo Nome esta vazio !");
+        }
+    } else {
+        swal("Campo E-mail esta vazio !");
+    }
+};
+
+btn.addEventListener('click', function () {
+    ClickEnvent(email, nome);
+})
+
+
+function ClickComprar() {
+    swal("FUNÇÃO AINDA NÃO CRIADA")
+}
+
+btnComprar.addEventListener('click', function () {
+    ClickComprar();
+});
